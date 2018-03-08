9cebbd558dac66a6f49fbb9ac464e91334d11535
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 10:27:36 -0600

    Create SavingsAccount.cs

---
+++ SavingsAccount.cs
@@ -0,0 +1,34 @@
+public class SavingsAccount
+{
+    public static decimal AnnualInterestRate { get; set; }
+
+    private decimal savingsBalance;
+
+    public SavingsAccount(decimal savingsBalance)
+    {
+        SavingsBalance = savingsBalance;
+    }
+
+    public decimal SavingsBalance
+    {
+        get
+        {
+            return savingsBalance;
+        }
+        set
+        {
+            if (value < 0)
+                savingsBalance = 0;
+            else
+                savingsBalance = value;
+        }
+    }
+
+    public void CalculateInterestRate()
+    {
+        SavingsBalance += SavingsBalance * (AnnualInterestRate / 12);
+    }
+
+    public override string ToString() =>
+        $"New Balance: {SavingsBalance:C}.";
+}//end class
