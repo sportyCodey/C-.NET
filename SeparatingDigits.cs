4cce61b4b8e0c3ed99ccf2acaa75b02e1a9ce5f7
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 10:36:16 -0600

    Create SeparatingDigits.cs

---
+++ SeparatingDigits.cs
@@ -0,0 +1,40 @@
+using System;
+
+class SeparatingDigits
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        int dummyNum = input;
+
+        int count = 0;
+        while (dummyNum != 0)
+        {
+            dummyNum /= 10;
+            ++count;
+        }
+
+        int index = count;
+        int counter = 0;
+        while(counter < count)
+        {
+            int temp = GetDigit(input, index - 1);
+            Console.Write(temp + "  ");
+            ++counter;
+            --index;
+        }
+        Console.WriteLine();
+    }
+
+    static int GetDigit(int input, int index)
+    {
+        int counter = 0;
+        while (counter < index)
+        {
+            input /= 10;
+            ++counter;
+        }
+        return input % 10;
+    }
+}
