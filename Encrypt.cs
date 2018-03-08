58e7f5d26015bcd757a22bcb2f0b2d0ff7a52457
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 10:33:15 -0600

    Create Encrypt.cs

---
+++ Encrypt.cs
@@ -0,0 +1,26 @@
+class Encrypt: DataManipulation
+{
+    public Encrypt()
+        : base() { }
+
+    public Encrypt(int num)
+        : base(num) { }
+
+    public override void ManipulateData()
+    {
+        int dummyNum = Num;
+        for (int i = encryptedData.Length - 1; i >= 0; i--)
+        {
+            int temp = dummyNum;
+            temp = dummyNum % 10;
+            temp += 7;
+            temp %= 10;
+
+            encryptedData[i] = temp;
+
+            dummyNum /= 10;
+        }
+        swap(0, 2);
+        swap(1, 3);
+    }
+}//end class
